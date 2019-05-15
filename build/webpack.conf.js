c62f33464debcc5aab95373a766ffa8aff399677
asmcos <asmcos@gmail.com>
2019-05-15 09:39:06 +0800

    push menu

--- build/webpack.conf.js
+++ build/webpack.conf.js
@@ -13,6 +13,7 @@ module.exports = {
 			 // Copy directory contents to {output}/to/directory/
             { from: './node_modules/admin-lte/dist',to: '../../public'},
             { from: './node_modules/bootstrap/dist',to: '../../public'},
+            { from: './node_modules/bootstrap/js/tooltip.js',to: '../../public/js/tooltip.js'},
             { from: './node_modules/vue/dist/vue.js',to: '../../public/js/vue.js'},
 				
 		], {
